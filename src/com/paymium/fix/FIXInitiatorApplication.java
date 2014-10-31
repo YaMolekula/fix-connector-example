28bdc335348f472a9e6a675f471ca864c3ad0110
davout <david.francois@webflows.fr>
2014-10-31 17:23:24 +0100

    Initial commit

---
+++ src/com/paymium/fix/FIXInitiatorApplication.java
@@ -0,0 +1,75 @@
+package com.paymium.fix;
+
+import quickfix.Application;
+import quickfix.DoNotSend;
+import quickfix.FieldNotFound;
+import quickfix.IncorrectDataFormat;
+import quickfix.IncorrectTagValue;
+import quickfix.Message;
+import quickfix.RejectLogon;
+import quickfix.SessionID;
+import quickfix.UnsupportedMessageType;
+import quickfix.field.MsgType;
+import quickfix.field.Username;
+
+public class FIXInitiatorApplication implements Application {
+
+
+	@Override
+	public void onCreate(SessionID sessionId) {
+		System.out.println("onCreate");
+	}
+
+	@Override
+	public void onLogon(SessionID sessionId) {
+		System.out.println("onLogon");
+	}
+
+	@Override
+	public void onLogout(SessionID sessionId) {
+		System.out.println("onLogout");
+	}
+
+	@Override
+	public void toAdmin(Message message, SessionID sessionId) {
+		
+	/*	System.out.println("To admin : " + message.toString());
+		
+		try {
+			if (MsgType.LOGON.equals(message.getHeader().getField(new MsgType()).getValue())) {	
+				message.getHeader().setField(new Username("JAVA_TESTS"));
+			}
+		}
+		catch(FieldNotFound fnfe) {
+			// NOOP
+			System.out.println("fuckendish");
+		}*/
+	}
+
+
+	@Override
+	public void fromAdmin(Message message, SessionID sessionId)
+			throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue,
+			RejectLogon {
+		System.out.println("ADM------------------------------------------------------");
+		System.out.println(message.toString());
+		System.out.println("ADM------------------------------------------------------");
+	}
+
+	@Override
+	public void toApp(Message message, SessionID sessionId) throws DoNotSend {
+		System.out.println("TO------------------------------------------------------");
+		System.out.println(message.toString());
+		System.out.println("TO------------------------------------------------------");
+	}
+
+	@Override
+	public void fromApp(Message message, SessionID sessionId)
+			throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue,
+			UnsupportedMessageType {
+		System.out.println("FROM------------------------------------------------------");
+		System.out.println(message.toString());
+		System.out.println("FROM------------------------------------------------------");
+
+	}
+}
---
+++ src/com/paymium/fix/Main.java
@@ -0,0 +1,60 @@
+package com.paymium.fix;
+
+import quickfix.Application;
+import quickfix.ConfigError;
+import quickfix.DefaultMessageFactory;
+import quickfix.FileStoreFactory;
+import quickfix.MessageFactory;
+import quickfix.ScreenLogFactory;
+import quickfix.Session;
+import quickfix.SessionID;
+import quickfix.SessionSettings;
+import quickfix.SocketInitiator;
+
+
+public class Main {
+
+	public static void main(String[] args) 
+			throws Exception {
+		System.out.println("Prutendafrischen");
+
+		SocketInitiator socketInitiator = null;
+		try {
+			SessionSettings initiatorSettings = new SessionSettings("/Users/david/code/java/fix-test/settings.cfg");
+			Application initiatorApplication = new FIXInitiatorApplication();
+			FileStoreFactory fileStoreFactory = new FileStoreFactory(initiatorSettings);
+			ScreenLogFactory fileLogFactory = new ScreenLogFactory(initiatorSettings);
+			MessageFactory messageFactory = new DefaultMessageFactory();
+			socketInitiator = new SocketInitiator(initiatorApplication, fileStoreFactory, initiatorSettings, fileLogFactory, messageFactory);
+			
+			System.out.println("************");
+			socketInitiator.start();
+			System.out.println("************");
+			
+			SessionID sessionId = socketInitiator.getSessions().get(0);
+			Session.lookupSession(sessionId).logon();
+			System.out.println("-------");
+			System.out.println(sessionId.toString());
+			System.out.println(Session.lookupSession(sessionId).toString());
+			System.out.println("-------");
+			Session.lookupSession(sessionId).logon();
+			
+			
+			//Logon logon = new Logon();
+			//Session.sendToTarget(logon, sessionId);
+			
+			//MarketDataRequest marketDataRequest = new MarketDataRequest();
+			//Session.sendToTarget(marketDataRequest, sessionId);
+			
+			
+			  System.in.read();
+			  socketInitiator.stop();
+			
+			
+		} catch (ConfigError e) {
+			System.out.println("whooooooops");
+			e.printStackTrace();
+		}
+	}
+
+}
