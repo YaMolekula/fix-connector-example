4c38223133e9fd50b16ca59da35fdfcce5b0b90f
davout <david.francois@webflows.fr>
2014-11-20 12:40:24 +0100

    Automatic session change is now correctly handled

--- src/com/paymium/fix/FIXInitiatorApplication.java
+++ src/com/paymium/fix/FIXInitiatorApplication.java
@@ -8,11 +8,21 @@ import quickfix.IncorrectTagValue;
 import quickfix.Message;
 import quickfix.RejectLogon;
 import quickfix.RuntimeError;
+import quickfix.Session;
 import quickfix.SessionID;
+import quickfix.SessionNotFound;
 import quickfix.UnsupportedMessageType;
+import quickfix.field.MDEntryType;
+import quickfix.field.MDReqID;
+import quickfix.field.MDUpdateType;
+import quickfix.field.MarketDepth;
 import quickfix.field.MsgType;
+import quickfix.field.NoMDEntryTypes;
 import quickfix.field.ResetSeqNumFlag;
+import quickfix.field.SubscriptionRequestType;
+import quickfix.field.Symbol;
 import quickfix.field.Username;
+import quickfix.fix44.MarketDataRequest;
 
 public class FIXInitiatorApplication implements Application {
 
@@ -26,12 +36,10 @@ public class FIXInitiatorApplication implements Application {
 		if (MsgType.MARKET_DATA_SNAPSHOT_FULL_REFRESH.equals(message.getHeader().getField(new MsgType()).getValue())) {
 			// Handle snapshot here
 			System.out.println("Received a full market data snapshot");
-			System.out.println("Place custom handling code here");
 		}
 		else if (MsgType.MARKET_DATA_INCREMENTAL_REFRESH.equals(message.getHeader().getField(new MsgType()).getValue())) {
 			// Handle incremental updates here
 			System.out.println("Received an incremental market data update");
-			System.out.println("Place custom handling code here");
 		}
 		
 	}
@@ -40,7 +48,14 @@ public class FIXInitiatorApplication implements Application {
 	public void onCreate(SessionID arg0) { }
 
 	@Override
-	public void onLogon(SessionID arg0)  { }
+	public void onLogon(SessionID sessionId)  { 
+		try {
+			sendMarketDataRequest(sessionId);
+		} catch (SessionNotFound e) {
+			// By definition this should never happen
+			System.out.println("Unable to find session <" + sessionId.toString() + ">");
+		}
+	}
 
 	@Override
 	public void onLogout(SessionID arg0)  { }
@@ -61,5 +76,40 @@ public class FIXInitiatorApplication implements Application {
 
 	@Override
 	public void toApp(Message arg0, SessionID arg1) throws DoNotSend  { }
+	
+	private void sendMarketDataRequest(SessionID sessionId) 
+			throws SessionNotFound {
+		
+		// We generate a new request ID
+		MDReqID reqId = new MDReqID("MDRQ-" + String.valueOf(System.currentTimeMillis()));
 
+		// We want EUR/XBT data
+		String currencyPair = "EUR/XBT";
+
+		// We want to get a snapshot and also subscribe to the market depth updates
+		SubscriptionRequestType subscriptionType = new SubscriptionRequestType(SubscriptionRequestType.SNAPSHOT_PLUS_UPDATES);
+
+		// We want the full book here, not just the top
+		MarketDepth depthType = new MarketDepth(1);
+
+		// We'll want only incremental refreshes when new data is available
+		MDUpdateType updateType = new MDUpdateType(MDUpdateType.INCREMENTAL_REFRESH);
+
+		MarketDataRequest mdr = new MarketDataRequest(reqId, subscriptionType, depthType);
+
+		mdr.setField(updateType);
+
+		MarketDataRequest.NoRelatedSym instruments = new MarketDataRequest.NoRelatedSym();
+		instruments.set(new Symbol(currencyPair));
+		mdr.addGroup(instruments);
+
+		// Specify that we'll want the bids and asks
+		mdr.setField(new NoMDEntryTypes(2));	
+		MarketDataRequest.NoMDEntryTypes group = new MarketDataRequest.NoMDEntryTypes();
+		group.set(new MDEntryType(MDEntryType.BID));
+		group.set(new MDEntryType(MDEntryType.OFFER));
+		mdr.addGroup(group);
+
+		Session.sendToTarget(mdr, sessionId);
+	}
 }
--- src/com/paymium/fix/Main.java
+++ src/com/paymium/fix/Main.java
@@ -7,19 +7,8 @@ import quickfix.DefaultMessageFactory;
 import quickfix.FileStoreFactory;
 import quickfix.MessageFactory;
 import quickfix.ScreenLogFactory;
-import quickfix.Session;
-import quickfix.SessionID;
-import quickfix.SessionNotFound;
 import quickfix.SessionSettings;
 import quickfix.SocketInitiator;
-import quickfix.field.MDEntryType;
-import quickfix.field.MDReqID;
-import quickfix.field.MDUpdateType;
-import quickfix.field.MarketDepth;
-import quickfix.field.NoMDEntryTypes;
-import quickfix.field.SubscriptionRequestType;
-import quickfix.field.Symbol;
-import quickfix.fix44.MarketDataRequest;
 
 
 public class Main {
@@ -43,54 +32,9 @@ public class Main {
 		socketInitiator = new SocketInitiator(initiatorApplication, fileStoreFactory, initiatorSettings, fileLogFactory, messageFactory);
 		socketInitiator.start();
 
-		SessionID sessionId = socketInitiator.getSessions().get(0);
-
-		// Give 15s to the engine for it to properly connect
-		Thread.sleep(15000);
-		
-		// At this point we're logged-on, time to send a market data request
-		sendMarketDataRequest(sessionId);
-
 		Thread.sleep(1000000);
 
 		System.out.println("Exited FIX initiator");
 	}
-
-	private static void sendMarketDataRequest(SessionID sessionId) 
-			throws SessionNotFound {
-		
-		// We generate a new request ID
-		MDReqID reqId = new MDReqID("MDRQ-" + String.valueOf(System.currentTimeMillis()));
-
-		// We want EUR/XBT data
-		String currencyPair = "EUR/XBT";
-
-		// We want to get a snapshot and also subscribe to the market depth updates
-		SubscriptionRequestType subscriptionType = new SubscriptionRequestType(SubscriptionRequestType.SNAPSHOT_PLUS_UPDATES);
-
-		// We want the full book here, not just the top
-		MarketDepth depthType = new MarketDepth(1);
-
-		// We'll want only incremental refreshes when new data is available
-		MDUpdateType updateType = new MDUpdateType(MDUpdateType.INCREMENTAL_REFRESH);
-
-		MarketDataRequest mdr = new MarketDataRequest(reqId, subscriptionType, depthType);
-
-		mdr.setField(updateType);
-
-		MarketDataRequest.NoRelatedSym instruments = new MarketDataRequest.NoRelatedSym();
-		instruments.set(new Symbol(currencyPair));
-		mdr.addGroup(instruments);
-
-		// Specify that we'll want the bids and asks
-		mdr.setField(new NoMDEntryTypes(2));	
-		MarketDataRequest.NoMDEntryTypes group = new MarketDataRequest.NoMDEntryTypes();
-		group.set(new MDEntryType(MDEntryType.BID));
-		group.set(new MDEntryType(MDEntryType.OFFER));
-		mdr.addGroup(group);
-
-		Session.sendToTarget(mdr, sessionId);
-	}
-
 
 }
